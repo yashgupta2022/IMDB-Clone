8af2531cf595f16b275239a609928218019b1645
YASH GUPTA <yash2002.gupta@gmail.com>
2024-01-17 21:58:01 +0530

    Cards added

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading(){
+    return(
+        <div className="flex justify-center mt-15">
+            <img className='h-50' src='Spinner-1s-96px.svg' alt= "loading... "/>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Results from '@/components/Results';
+import { revalidateTag } from 'next/cache';
 import React from 'react'
 
 const API_KEY = process.env.API_KEY;
@@ -9,6 +10,7 @@ export default async function Home({searchParams}) {
     `https://api.themoviedb.org/3${
       genre==='fetchTopRated'?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
   )
+  
   const data = await res.json()
   
     if (!res.ok){
